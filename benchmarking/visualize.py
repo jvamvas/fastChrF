da1b12fb3818a43328dce89f9701da31fb3fa73d
Jannis Vamvas <vamvas@cl.uzh.ch>
2024-01-02 11:23:22 +0100

    Fix benchmark figure

--- benchmarking/visualize.py
+++ benchmarking/visualize.py
@@ -9,9 +9,9 @@ with open(Path(__file__).parent / "results.tsv") as f:
     data = list(reader)
 
 hypotheses = [int(row["n"]) for row in data]
-sacreBLEU = 100 * np.array([float(row["sacrebleu"]) for row in data])
-fastchrf_pairwise = 100 * np.array([float(row["pairwise"]) for row in data])
-fastchrf_aggregate = 100 * np.array([float(row["aggregate"]) for row in data])
+sacreBLEU = 1000 * np.array([float(row["sacrebleu"]) for row in data])
+fastchrf_pairwise = 1000 * np.array([float(row["pairwise"]) for row in data])
+fastchrf_aggregate = 1000 * np.array([float(row["aggregate"]) for row in data])
 
 plt.rcParams.update({'font.size': 18})
 plt.figure(figsize=(10, 6))
@@ -21,12 +21,12 @@ plt.plot(hypotheses, fastchrf_pairwise, label='fastchrf.pairwise_chrf', marker='
 plt.plot(hypotheses, fastchrf_aggregate, label='fastchrf.aggregate_chrf', marker='^')
 
 plt.xlabel('Number of Hypotheses (log scale)')
-plt.ylabel('Time in milliseconds (log scale)')
+plt.ylabel('Time (log scale)')
 plt.xscale('log')
 plt.yscale('log')
 plt.xticks(hypotheses, labels=[f"{h}" for h in hypotheses])
-plt.yticks([0.1, 1, 10, 100, 1000, 10000], labels=[f"{t}" for t in [0.1, 1, 10, 100, 1000, 10000]])
+plt.yticks([0.1, 1, 10, 100, 1000, 10000, 100000], labels=["0.1 ms", "1 ms", "10 ms", "100 ms", "1 s", "10 s", "100 s"])
 
 plt.legend()
 
-plt.savefig(Path(__file__).parent / "results.png", dpi=60)
+plt.savefig(Path(__file__).parent / "results.png", dpi=60, bbox_inches='tight')
